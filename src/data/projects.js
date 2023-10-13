79a18fd1848b1ffd5e6672689d5f904a9772ce57
fedeparrilla10 <federicoivanpd@gmail.com>
2023-10-13 08:58:19 +0300

    [add] specs section + fix buysection + footer

---
+++ src/data/projects.js
@@ -0,0 +1,27 @@
+export const allProjects = [
+  {
+    name: 'RecoFM',
+    img: '../../public/projects/RecoFM.png',
+    description:
+      'Responsive SPA developed with React. Animations powered by the Framer Motion library. BEM methodology for proper and organised styling. Random data block generator. Routing implemented using React Router DOM.',
+    tech: ['../../public/icons/react.svg', '../../public/icons/css3.svg'],
+  },
+  {
+    name: 'BookFinder',
+    img: '../../public/projects/BookFinder.png',
+    description:
+      'SPA developed with React with responsive design, registration, login, and CSS using BEM methodology. It leverages useState, useEffect, useContext, and useParams hooks for functionality and Axios for requests and modifications.',
+    tech: ['../../public/icons/react.svg', '../../public/icons/css3.svg'],
+  },
+  {
+    name: 'DestinosAR',
+    img: '../../public/projects/DestinosAR.png',
+    description:
+      'SPA developed with Angular with comprehensive CRUD, complete with user registration and login features. It leverages guards, interceptors, switchMap, and reactive forms. Additionally, it employs pipes to handle data filtering.',
+    tech: [
+      '../../public/icons/angular.svg',
+      '../../public/icons/typescript.svg',
+      '../../public/icons/sass.svg',
+    ],
+  },
+];
---
+++ src/data/stack.js
@@ -0,0 +1,50 @@
+export const allTechs = [
+  {
+    name: 'HTML5',
+    img: '../../public/icons/html5.svg',
+  },
+  {
+    name: 'CSS3',
+    img: '../../public/icons/css3.svg',
+  },
+  {
+    name: 'SASS',
+    img: '../../public/icons/sass.svg',
+  },
+  {
+    name: 'JavaScript',
+    img: '../../public/icons/javascript.svg',
+  },
+  {
+    name: 'TypeScript',
+    img: '../../public/icons/typescript.svg',
+  },
+  {
+    name: 'React',
+    img: '../../public/icons/react.svg',
+  },
+  {
+    name: 'NextJS',
+    img: '../../public/icons/nextjs.svg',
+  },
+  {
+    name: 'Angular',
+    img: '../../public/icons/angular.svg',
+  },
+  {
+    name: 'PHP',
+    img: '../../public/icons/php.svg',
+  },
+  {
+    name: 'Laravel',
+    img: '../../public/icons/laravel.svg',
+  },
+  {
+    name: 'MUI',
+    img: '../../public/icons/mui.svg',
+  },
+  {
+    name: 'TailwindCSS',
+    img: '../../public/icons/tailwind.svg',
+  },
+];
