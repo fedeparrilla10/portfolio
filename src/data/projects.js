0b387685de2e5b971a0835aa1227c6a7b2c05131
fedeparrilla10 <federicoivanpd@gmail.com>
2024-01-30 21:52:23 +0200

    [update] hero + projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,27 +1,23 @@
 export const allProjects = [
   {
     name: 'RecoFM',
-    img: '../../public/projects/RecoFM.png',
+    img: '/projects/RecoFM.png',
     description:
       'Responsive SPA developed with React. Animations powered by the Framer Motion library. BEM methodology for proper and organised styling. Random data block generator. Routing implemented using React Router DOM.',
-    tech: ['../../public/icons/react.svg', '../../public/icons/css3.svg'],
+    tech: ['/icons/react.svg', '/icons/css3.svg'],
   },
   {
     name: 'BookFinder',
-    img: '../../public/projects/BookFinder.png',
+    img: '/projects/bookfinder.png',
     description:
       'SPA developed with React with responsive design, registration, login, and CSS using BEM methodology. It leverages useState, useEffect, useContext, and useParams hooks for functionality and Axios for requests and modifications.',
-    tech: ['../../public/icons/react.svg', '../../public/icons/css3.svg'],
+    tech: ['/icons/react.svg', '/icons/css3.svg'],
   },
   {
     name: 'DestinosAR',
-    img: '../../public/projects/DestinosAR.png',
+    img: '/projects/DestinosAR.png',
     description:
       'SPA developed with Angular with comprehensive CRUD, complete with user registration and login features. It leverages guards, interceptors, switchMap, and reactive forms. Additionally, it employs pipes to handle data filtering.',
-    tech: [
-      '../../public/icons/angular.svg',
-      '../../public/icons/typescript.svg',
-      '../../public/icons/sass.svg',
-    ],
+    tech: ['/icons/angular.svg', '/icons/typescript.svg', '/icons/sass.svg'],
   },
 ];
