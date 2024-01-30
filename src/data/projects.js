83ff59c934afa30280a942b6fac22165aabab358
fedeparrilla10 <federicoivanpd@gmail.com>
2024-01-30 22:02:12 +0200

    [update] portfolio routes

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,27 +1,27 @@
 export const allProjects = [
   {
     name: 'RecoFM',
-    img: 'portfolio/projects/RecoFM.png',
+    img: '/portfolio/projects/RecoFM.png',
     description:
       'Responsive SPA developed with React. Animations powered by the Framer Motion library. BEM methodology for proper and organised styling. Random data block generator. Routing implemented using React Router DOM.',
-    tech: ['portfolio/icons/react.svg', '/icons/css3.svg'],
+    tech: ['/portfolio/icons/react.svg', '/icons/css3.svg'],
   },
   {
     name: 'BookFinder',
-    img: 'portfolio/projects/bookfinder.png',
+    img: '/portfolio/projects/bookfinder.png',
     description:
       'SPA developed with React with responsive design, registration, login, and CSS using BEM methodology. It leverages useState, useEffect, useContext, and useParams hooks for functionality and Axios for requests and modifications.',
-    tech: ['portfolio/icons/react.svg', 'portfolio/icons/css3.svg'],
+    tech: ['/portfolio/icons/react.svg', '/portfolio/icons/css3.svg'],
   },
   {
     name: 'DestinosAR',
-    img: 'portfolio/projects/DestinosAR.png',
+    img: '/portfolio/projects/DestinosAR.png',
     description:
       'SPA developed with Angular with comprehensive CRUD, complete with user registration and login features. It leverages guards, interceptors, switchMap, and reactive forms. Additionally, it employs pipes to handle data filtering.',
     tech: [
-      'portfolio/icons/angular.svg',
-      'portfolio/icons/typescript.svg',
-      'portfolio/icons/sass.svg',
+      '/portfolio/icons/angular.svg',
+      '/portfolio/icons/typescript.svg',
+      '/portfolio/icons/sass.svg',
     ],
   },
 ];
--- src/data/stack.js
+++ src/data/stack.js
@@ -1,50 +1,50 @@
 export const allTechs = [
   {
     name: 'HTML5',
-    img: 'portfolio/icons/html5.svg',
+    img: '/portfolio/icons/html5.svg',
   },
   {
     name: 'CSS3',
-    img: 'portfolio/icons/css3.svg',
+    img: '/portfolio/icons/css3.svg',
   },
   {
     name: 'SASS',
-    img: 'portfolio/icons/sass.svg',
+    img: '/portfolio/icons/sass.svg',
   },
   {
     name: 'JavaScript',
-    img: 'portfolio/icons/javascript.svg',
+    img: '/portfolio/icons/javascript.svg',
   },
   {
     name: 'TypeScript',
-    img: 'portfolio/icons/typescript.svg',
+    img: '/portfolio/icons/typescript.svg',
   },
   {
     name: 'React',
-    img: 'portfolio/icons/react.svg',
+    img: '/portfolio/icons/react.svg',
   },
   {
     name: 'NextJS',
-    img: 'portfolio/icons/nextjs.svg',
+    img: '/portfolio/icons/nextjs.svg',
   },
   {
     name: 'Angular',
-    img: 'portfolio/icons/angular.svg',
+    img: '/portfolio/icons/angular.svg',
   },
   {
     name: 'PHP',
-    img: 'portfolio/icons/php.svg',
+    img: '/portfolio/icons/php.svg',
   },
   {
     name: 'Laravel',
-    img: 'portfolio/icons/laravel.svg',
+    img: '/portfolio/icons/laravel.svg',
   },
   {
     name: 'MUI',
-    img: 'portfolio/icons/mui.svg',
+    img: '/portfolio/icons/mui.svg',
   },
   {
     name: 'TailwindCSS',
-    img: 'portfolio/icons/tailwind.svg',
+    img: '/portfolio/icons/tailwind.svg',
   },
 ];
