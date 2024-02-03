5aa827598d982baccef99aced9808d27fa25eb47
fedeparrilla10 <federicoivanpd@gmail.com>
2024-02-03 20:07:00 +0200

    [add] translations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
-
+import astroI18next from 'astro-i18next';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fedeparrilla10.github.io',
   base: '/portfolio',
-  integrations: [tailwind()],
+  integrations: [tailwind(), astroI18next()],
 });
---
+++ src/i18n/ui.ts
@@ -0,0 +1,75 @@
+export const languages = {
+  en: 'English',
+  es: 'Español',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'nav.home': 'Home',
+    'nav.projects': 'Projects',
+    'nav.contact': 'Contact',
+
+    'hero.hello': "Hello!👋🏻 I'm ",
+    'hero.job': 'a Software Developer',
+    'hero.bio': `I'm a developer who embarked on their journey in October 2022 as a self-taught enthusiast. I later expanded my knowledge by completing a bootcamp at UpgradeHub, with a specific focus on front-end development. Dedication and enthusiasm have been with me from the beginning, and I view them as my core foundations.`,
+
+    'work.title': 'Work Experience',
+    'work.work': 'Full-Stack Developer in Sitelicon Ecommerce Services',
+    'work.date': 'August 2023 - Present',
+    'work.functions.first': `Developed frontend solutions using various frameworks and technologies, such as React, NextJS or Astro, implementing interactive components to improve user experiences.`,
+    'work.functions.second': `Designed and crafted multiple landing pages for Nacon, a renowned mark in the entertainment industry, ensuring timely delivery and efficient development by creating a custom template.`,
+    'work.functions.third': `Led the development of distinct sections for PACOMARTÍNEZ, a prominent travel-related products retailer. Played a key role in creating both intuitive admin panels and user interfaces.`,
+    'work.functions.fourth': `Collaborated in the creation of robust RESTful APIs using Laravel (PHP). Actively contributed to the development process by creating endpoints and managing data effectively.`,
+
+    'study.date': 'October 2022 - July 2023',
+    'study.title': 'Front-End Development Bootcamp in UpgradeHub',
+    'study.description':
+      'Worked extensively with JavaScript, Angular, and React. Successfully completed challenging projects, gaining valuable hands-on experience.',
+
+    'projects.title': 'Projects',
+
+    'contact.title': 'Contact',
+    'contact.email': 'Email',
+    'contact.subject': 'Subject',
+    'contact.message': 'Message',
+    'contact.submit': 'Send message',
+    'contact.placeholder.email': 'name@something.com',
+    'contact.placeholder.subject': 'Subject',
+    'contact.placeholder.message': 'Write your message here',
+  },
+  es: {
+    'nav.home': 'Home',
+    'nav.projects': 'Proyectos',
+    'nav.contact': 'Contacto',
+
+    'hero.hello': '¡Hola!👋🏻 Soy ',
+    'hero.job': 'un Desarrollador de Software',
+    'hero.bio': `Soy un desarrollador que inició su viaje en octubre de 2022 como entusiasta autodidacta. Más tarde, amplié mis conocimientos al completar un bootcamp en UpgradeHub, con un enfoque específico en desarrollo front-end. La dedicación y el entusiasmo me han acompañado desde el principio, y los considero como mis bases fundamentales.`,
+
+    'work.title': 'Experiencia Laboral',
+    'work.work': 'Full-Stack Developer en Sitelicon Ecommerce Services',
+    'work.date': 'Agosto 2023 - Presente',
+    'work.functions.first': `Desarrollo de soluciones front-end utilizando varios frameworks y tecnologías como React, NextJS o Astro, implementando componentes interactivos para mejorar la experiencia del usuario.`,
+    'work.functions.second': `Encabecé el desarrollo de múltiples landings para Nacon, una marca reconocida en la industria del entretenimiento, asegurando la entrega oportuna y un desarrollo eficiente mediante la creación de una plantilla personalizada.`,
+    'work.functions.third': `Me encargué del desarrollo de diferentes secciones para PACOMARTÍNEZ, un destacado minorista de productos relacionados con viajes. Trabajé en la creación de paneles de administración intuitivos e interfaces de usuario.`,
+    'work.functions.fourth': `Colaboré en la creación de API RESTful utilizando Laravel (PHP). Contribuí activamente al proceso de desarrollo mediante la creación de puntos finales y la gestión eficaz de datos.`,
+
+    'study.date': 'Octubre 2022 - Julio 2023',
+    'study.title': 'Front-End Development Bootcamp en UpgradeHub',
+    'study.description':
+      'Perfeccioné mis habilidades trabajando extensamente con tecnologías como JavaScript, Angular y React. Completé con éxito proyectos desafiantes, adquiriendo una valiosa experiencia práctica.',
+
+    'projects.title': 'Proyectos',
+
+    'contact.title': 'Contacto',
+    'contact.email': 'Email',
+    'contact.subject': 'Asunto',
+    'contact.message': 'Mensaje',
+    'contact.submit': 'Enviar mensaje',
+    'contact.placeholder.email': 'nombre@algo.com',
+    'contact.placeholder.subject': 'Asunto',
+    'contact.placeholder.message': 'Escribe tu mensaje aquí',
+  },
+} as const;
