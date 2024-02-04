72da33667711cd01f944003d01385d3f1df1d6da
fedeparrilla10 <federicoivanpd@gmail.com>
2024-02-04 17:07:15 +0200

    [fix] utils.ts

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,7 +1,7 @@
 import { ui, defaultLang } from './ui';
 
 export function getLangFromUrl(url: URL) {
-  const [, lang] = url.pathname.split('/');
+  const [, lang] = url.pathname.split('/portfolio/');
   if (lang in ui) return lang as keyof typeof ui;
   return defaultLang;
 }
