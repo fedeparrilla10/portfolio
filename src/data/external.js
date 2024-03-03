25b7a91d2990065ac02f37cd8add1b0caa966118
fedeparrilla10 <federicoivanpd@gmail.com>
2024-03-03 20:07:11 +0200

    [update] optimize code + update spanish CV

---
+++ src/data/external.js
@@ -0,0 +1,27 @@
+export const externalLinks = {
+  rrss: [
+    {
+      name: 'GitHub',
+      link: 'https://github.com/fedeparrilla10',
+      icon: '/portfolio/rrss/github.svg',
+    },
+    {
+      name: 'LinkedIn',
+      link: 'https://www.linkedin.com/in/fedeparrilla/',
+      icon: '/portfolio/rrss/linkedin.svg',
+    },
+  ],
+
+  cv: {
+    name: 'CV',
+    icon: '/portfolio/rrss/resume.svg',
+    es: '/portfolio/FedeParrillaES.pdf',
+    en: '/portfolio/FedeParrillaEN.pdf',
+  },
+
+  email: {
+    name: 'Email',
+    link: 'mailto:fedeparrilla.dev@gmail.com',
+    icon: '/portfolio/rrss/email.svg',
+  },
+};
