d52cddfab3942b3d53e8e09785ec3fac87c5e581
fedeparrilla10 <federicoivanpd@gmail.com>
2024-03-04 18:34:59 +0200

    coso

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,4 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://fedeparrilla10.github.io',
-  base: '/portfolio',
-  trailingSlash: 'never',
 });
